bfe88f90487bf1e60a1a31d548e48546ded00b51
krocki <kamil.rocki@gmail.com>
2019-12-29 10:40:06 -0800

    fix linux v

--- randnf.c
+++ randnf.c
@@ -1,5 +1,9 @@
 #include <math.h>
 #include <stdlib.h>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846264338327950288
+#endif
+
 float randnf(float mean, float std) {
     float  x = (float)rand() / RAND_MAX,
            y = (float)rand() / RAND_MAX,
